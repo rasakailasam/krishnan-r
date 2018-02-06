dda0ad7c7dfcc53204979e62e0dd84fc5e899654
rasakailasam <35728308+rasakailasam@users.noreply.github.com>
2018-02-06 16:39:06 +0530

    Create set2q14.c

---
+++ set2q14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+clrscr();
+printf("Enter n values");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+if(1%2==1)
+{
+printf("%d ODD \n",i);
+}
+}
+getch();
+}
